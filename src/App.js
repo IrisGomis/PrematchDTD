518bf3fc33e7e48d2f290768bcff2a3336671e69
natalia <natiriarte5@gmail.com>
2023-03-29 14:48:59 +0200

    changes drawer

--- src/App.js
+++ src/App.js
@@ -1,13 +1,15 @@
 import './App.css';
 import 'tailwindcss/tailwind.css';
-import MolForm from './components/molecule/MolForm';
+//import MolForm from './components/molecule/MolForm';
+import Drawer from './components/atome/drawer/Drawer';
 
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       <MolForm/>
+       {/* <MolForm/> */}
+       <Drawer />
       </header>
     </div>
   );
--- src/components/atome/drawer/Drawer.jsx
+++ src/components/atome/drawer/Drawer.jsx
@@ -143,9 +143,7 @@ const Drawer = () => {
       </li>
     </ul>
   </nav>
-  <!-- Sidenav -->
-
-  <!-- Toggler -->
+  
   <button
     class="mt-10 inline-block rounded bg-blue-600 px-6 py-2.5 text-xs font-medium uppercase leading-tight text-white shadow-md transition duration-150 ease-in-out hover:bg-blue-700 hover:shadow-lg focus:bg-blue-700 focus:shadow-lg focus:outline-none focus:ring-0 active:bg-blue-800 active:shadow-lg"
     data-te-sidenav-toggle-ref
@@ -165,7 +163,7 @@ const Drawer = () => {
       </svg>
     </span>
   </button>
-  <!-- Toggler -->
+
 </section>
   )
 }
