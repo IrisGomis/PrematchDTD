c26abf90c9535433bd34f99cb1047486eefdeb56
natalia <natiriarte5@gmail.com>
2023-03-29 14:44:58 +0200

    first drawer

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import './App.css';
+import './components/atome/css/styles.css';
+import Drawer from './components/atome/drawer/Drawer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <Drawer />
       </header>
     </div>
   );
