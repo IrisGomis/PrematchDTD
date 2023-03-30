29654a5821c3c7615a31a96da8f5ce65e48867f8
DianaB177 <anaidb77@gmail.com>
2023-03-30 13:22:26 +0200

    fixed colors

--- src/components/molecule/MolForm.jsx
+++ src/components/molecule/MolForm.jsx
@@ -18,9 +18,9 @@ export default function MolForm() {
             </div>
           </div>
           <div className="flex justify-center  border-b border-orange pb-6">
-            <p className="text-start text-gray-900 text-sm mr-8 pt-4">Hora</p>
+            <p className="text-start text-stone6 text-sm mr-8 pt-4">Hora</p>
             <DatePicker />
-            <p className="text-start text-gray-500 text-sm pt-4 my-3 mx-3">
+            <p className="text-start text-stone6 text-sm pt-4 my-3 mx-3">
               To
             </p>
             <DatePicker />
@@ -31,7 +31,7 @@ export default function MolForm() {
               <div className="sm:col-span-4">
                 <div className="mt-2">
                   <div className="flex rounded-md shadow-sm ring-1 ring-inset ring-stone4 focus-within:ring-2 focus-within:ring-inset focus:ring-orange sm:max-w-md">
-                    <span className="flex select-none items-center pl-3 text-gray-500 sm:text-sm">
+                    <span className="flex select-none items-center mr-8 pl-3 text-stone6 sm:text-sm">
                       Orgaizador
                     </span>
                     <input
@@ -39,7 +39,7 @@ export default function MolForm() {
                       name="username"
                       id="username"
                       autoComplete="username"
-                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-gray-900 placeholder:text-gray-400 focus:ring-0 sm:text-sm sm:leading-6"
+                      className="block flex-1 border-0 bg-transparent py-1.5 pl-1 text-stone8 placeholder:text-stone4 focus:ring-0 sm:text-sm sm:leading-6"
                       placeholder="  Somos-F5"
                     />
                   </div>
@@ -51,7 +51,7 @@ export default function MolForm() {
             <div className="sm:col-span-4">
               <label
                 htmlFor="email"
-                className="block text-start text-gray-900 text-sm mr-8 pt-4"
+                className="block text-start text-stone6 text-sm mr-8 pt-4"
               >
                 URL
               </label>
@@ -60,16 +60,16 @@ export default function MolForm() {
                   id="url"
                   name="url"
                   type="url"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-stone8 shadow-sm ring-1 ring-inset ring-strone4 focus:ring-2 focus:ring-inset focus:ring-orange sm:text-sm sm:leading-6"
                 />
               </div>
             </div>
           </div>
           <div className="border-b border-orange pb-12">
-            <div className="sm:col-span-3">
+            <div className="sm:col-span-3 flex">
               <label
                 htmlFor="country"
-                className="block text-start text-gray-900 text-sm mr-8 pt-4"
+                className="block text-start text-stone6 text-sm mr-8 pt-4"
               >
                 Asistentes
               </label>
@@ -78,7 +78,7 @@ export default function MolForm() {
                   id="country"
                   name="country"
                   autoComplete="country-name"
-                  className="block w-full rounded-md border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-inset focus:ring-orange-600 sm:max-w-xs sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 py-1.5 text-stone6 shadow-sm ring-1 ring-inset ring-stone3 focus:ring-2 focus:ring-inset focus:ring-orange sm:max-w-xs sm:text-sm sm:leading-6"
                 >
                   <option>NttData</option>
                   <option>FEM-CODERS-NORTE</option>
@@ -91,7 +91,7 @@ export default function MolForm() {
             <div>
               <label
                 htmlFor="comment"
-                className="block text-sm font-medium leading-6 text-gray-900"
+                className="block text-sm font-medium leading-6 text-stone6"
               >
                 Add your comment
               </label>
@@ -100,7 +100,7 @@ export default function MolForm() {
                   rows={4}
                   name="comment"
                   id="comment"
-                  className="block w-full rounded-md border-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:py-1.5 sm:text-sm sm:leading-6"
+                  className="block w-full rounded-md border-0 text-stone8 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-stone4 focus:ring-2 focus:ring-inset focus:ring-orange sm:py-1.5 sm:text-sm sm:leading-6"
                   defaultValue={""}
                 />
               </div>
@@ -110,13 +110,13 @@ export default function MolForm() {
           <div className="mt-6 flex items-center justify-end gap-x-6">
             <button
               type="button"
-              className="text-sm font-semibold leading-6 text-gray-900"
+              className="rounded-md bg-stone6 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-stone3 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-stone4"
             >
               Cancel
             </button>
             <button
               type="submit"
-              className="rounded-md bg-indigo-600 py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+              className="rounded-md bg-orange py-2 px-3 text-sm font-semibold text-white shadow-sm hover:bg-orangel focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
             >
               Save
             </button>
