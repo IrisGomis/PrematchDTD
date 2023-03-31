37956aef1bf14459f01949251010214b6a8ee86f
natalia <natiriarte5@gmail.com>
2023-03-31 12:11:24 +0200

    drawer finish

--- src/components/atome/drawer/Drawer.jsx
+++ src/components/atome/drawer/Drawer.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import "../css/styles.css";
 import SomosF5LogoWhite from '../../../assets/img/SomosF5LogoWhite.png';
 import {
   CalendarIcon,
@@ -27,43 +28,42 @@ const Drawer = () => {
     return classes.filter(Boolean).join(' ')
   }
   return (
-    <div className="flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-orange px-6 h-screen">
-    <div className="flex h-16 shrink-0 items-center">
-    <img
-           className="h-8 w-auto"
-           src={SomosF5LogoWhite}
-           alt="Logo de SomosF5"
-         />
-    </div>
-    <nav className="flex flex-1 flex-col">
-    <ul role="list" className="flex flex-1 flex-col gap-y-7">
-    <li>
-    <ul role="list" className="-mx-2 space-y-1">
-    {navigation.map((item) => (
-    <li key={item.name}>
-    <a
-  href={item.href}
-  className={classNames(
-    item.current
-      ? 'bg-gray-50 text-indigo-600'
-      : 'text-gray-700 hover:text-indigo-600 hover:bg-gray-50',
-    'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-white'
-  )}
->
-    <item.icon
-    className={classNames(
-      item.current ? 'text-indigo-600' : 'text-white group-hover:text-indigo-600',
-    'h-6 w-6 shrink-0'
-    )}
-    aria-hidden="true"
-    />
-    {item.name}
-    {item.count ? (
-    <span
-    className="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-white px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-600 ring-1 ring-inset ring-gray-200"
+    <div className="drawer-component flex grow flex-col gap-y-5 overflow-y-auto border-r border-gray-200 bg-orange px-6 h-screen">
+      <div className="flex h-16 shrink-0 items-center">
+        <img
+          className="h-8 w-auto"
+          src={SomosF5LogoWhite}
+          alt="Logo de SomosF5"
+        />
+      </div>
+      <nav className="flex flex-1 flex-col">
+        <ul role="list" className="flex flex-1 flex-col gap-y-7">
+          <li>
+            <ul role="list" className="names -mx-2 space-y-1">
+              {navigation.map((item) => (
+                <li key={item.name}>
+                  <a
+                    href={item.href}
+                    className={classNames(
+                      item.current
+                        ? 'bg-gray-50 text-white'
+                        : 'text-white hover:text-orangel hover:bg-stone5',
+                      'group flex gap-x-3 rounded-md p-2 text-sm leading-6 font-semibold text-white'
+                    )}
+                  >
+                    <item.icon
+                      className={classNames(
+                        item.current ? 'text-white' : 'text-white group-hover:text-indigo-600',
+                        'h-6 w-6 shrink-0'
+                      )}
+                      aria-hidden="true"
+                    />
+                    {item.name}
+                    {item.count ? (
+                      <span
+                        className="ml-auto w-9 min-w-max whitespace-nowrap rounded-full bg-white px-2.5 py-0.5 text-center text-xs font-medium leading-5 text-gray-600 ring-1 ring-inset ring-gray-200"
                         aria-hidden="true"
                       >
-
                         {item.count}
                       </span>
                     ) : null}
