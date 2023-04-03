9ef3fcc516a622b391d828db1284cc4f1e89bf12
IrisGomis <gomis.f.iris@gmai.com>
2023-04-03 09:08:21 +0200

    added view Home to App.js

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
 //import './App.css';
 import 'tailwindcss/tailwind.css';
-import AtomLogin from './components/atome/header/AtomLogin';
+import Home from './components/views/Home';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-       
+        <Home />       
       </header>
     </div>
   );
