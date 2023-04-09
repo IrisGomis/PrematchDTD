534c2ccb015d358149970096bbc40aab665b9452
DianaB177 <anaidb77@gmail.com>
2023-04-09 16:35:23 +0200

    mol-coder doing

--- src/router/Routes.jsx
+++ src/router/Routes.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import MolTableEvent from '../components/molecule/mol-event/MolTableEvent';
-import MolFormEvent from '../components/molecule/mol-event/MolFormEvent';
+import MolTableEventShowDelete from '../components/molecule/mol-event/MolTableEventShowDelete';
+import MolFormEventCreate from '../components/molecule/mol-event/MolFormEventCreate';
 import MolFormEventEdit from '../components/molecule/mol-event/MolFormEventEdit';
 import Layout from '../components/layout/Layout';
 import Home from '../components/views/Home';
@@ -17,14 +17,14 @@ function App() {
        <Router>
           <Layout>
             <Routes>
-            <Route path="/" element={<MolFormEvent />} />
+            <Route path="/" element={<MolFormEventCreate />} />
             <Route path="/home" element={<Home />} />
             <Route path="/admin" element={<Admin />} />
             <Route path="/taks" element={<Tareas />} />
             <Route path="/schools" element={<Schools />} />
             <Route path="/companies" element={<Companies />} />
             <Route path="/statistics" element={<Statistics />} />
-            <Route path="/table" element={<MolTableEvent />} />
+            <Route path="/table" element={<MolTableEventShowDelete />} />
             <Route path="/edit/:id" element={<MolFormEventEdit />} />
             </Routes>
           </Layout>
