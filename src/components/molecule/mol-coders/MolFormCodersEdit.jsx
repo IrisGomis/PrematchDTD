6eb1a961e9a58c1e89ad99d07dd71cea36917be2
DianaB177 <anaidb77@gmail.com>
2023-04-13 14:54:02 +0200

    update

--- src/components/molecule/mol-coders/MolFormCodersEdit.jsx
+++ src/components/molecule/mol-coders/MolFormCodersEdit.jsx
@@ -1,31 +1,19 @@
-import React, { useState, useEffect, Fragment } from "react";
+import React, { useState, useEffect } from "react";
 import { useParams, useNavigate } from "react-router-dom";
 import { getCodersById, updateCoders } from "../../../service/CodersService";
 import Swal from "sweetalert2";
-// import { Listbox, Transition } from "@headlessui/react";
-// import { CheckIcon, ChevronUpDownIcon } from "@heroicons/react/20/solid";
+import { getPromotions } from "../../../service/PromotionsServices";
+import { getEvento } from "../../../service/EventService";
 
-const MolFormCodersEdit = ({ event }) => {
-  // const people = [
-  //   { id: 1, name: "Wade Cooper" },
-  //   { id: 2, name: "Arlene Mccoy" },
-  //   { id: 3, name: "Devon Webb" },
-  //   { id: 4, name: "Tom Cook" },
-  //   { id: 5, name: "Tanya Fox" },
-  //   { id: 6, name: "Hellen Schmidt" },
-  //   { id: 7, name: "Caroline Schultz" },
-  //   { id: 8, name: "Mason Heaney" },
-  //   { id: 9, name: "Claudie Smitham" },
-  //   { id: 10, name: "Emil Schaefer" },
-  // ];
+const MolFormCodersEdit = ({ prop }) => {
+ 
+
   const { id } = useParams();
   const navigate = useNavigate();
-
-  //const [selected, setSelected] = useState("");
- 
+  const [event, setEvent] = useState([]);
+  const [promotions, setPromotions] = useState([]);
   const [event_id, setEventId] = useState(undefined);
   const [promo_id, setPromoId] = useState(undefined);
-  const [province_id, setProvinceId] = useState(undefined);
   const [name, setName] = useState(undefined);
   const [gender, setGender] = useState(undefined);
   const [years, setYears] = useState(undefined);
@@ -44,7 +32,6 @@ const MolFormCodersEdit = ({ event }) => {
         const { data } = await getCodersById(id);
         setEventId(data.event_id);
         setPromoId(data.promo_id);
-        setProvinceId(data.province_id);
         setName(data.name);
         setGender(data.gender);
         setYears(data.years);
@@ -65,7 +52,7 @@ const MolFormCodersEdit = ({ event }) => {
   const handleSubmit = async (event) => {
     event.preventDefault();
     event.preventDefault();
-    if ([event_id, promo_id, province_id, name, gender, years, avaliability, remote, email, phone, linkedin, github].some((value) => value === undefined)) {
+    if ([event_id, promo_id, name, gender, years, avaliability, remote, email, phone, linkedin, github].some((value) => value === undefined)) {
       Swal.fire({
         position: "center",
         icon: "error",
@@ -79,7 +66,6 @@ const MolFormCodersEdit = ({ event }) => {
       const coderData = {
         event_id,
         promo_id,
-        province_id,
         name,
         gender,
         years,
@@ -114,9 +100,21 @@ const MolFormCodersEdit = ({ event }) => {
     }
   };
 
-  // function classNames(...classes) {
-  //   return classes.filter(Boolean).join(" ");
-  // }
+  useEffect(() => {
+    getEvento()
+      .then((response) => {
+        setEvent(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    getPromotions()
+      .then((response) => {
+        setPromotions(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
 
  
   return (
@@ -127,102 +125,17 @@ const MolFormCodersEdit = ({ event }) => {
         <form className="bg-stone6" onSubmit={handleSubmit}>
           <div className="mt-10 space-y-8 border-b border-orange pb-12 sm:space-y-0 sm:divide-y sm:divide-orange sm:border-t sm:pb-0">
 
-          {/* <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-              <label
-                htmlFor="country"
-                className="block text-sm font-medium leading-6  text-white sm:pt-1.5"
-              >
-                Asistentes
-              </label>
-              <div className="mt-2 sm:col-span-2 sm:mt-0">
-                <Listbox value={selected} onChange={setSelected}>
-                  {({ open }) => (
-                    <>
-                      
-                      <div className="relative mt-2">
-                        <Listbox.Button className="relative w-full cursor-default rounded-md py-1.5 pl-3 pr-10 text-left shadow-sm ring-1 ring-inset ring-orange focus:outline-none focus:ring-2 focus:ring-orangel sm:text-sm sm:leading-6">
-                          <span className="block truncate">
-                            {selected.name}
-                          </span>
-                          <span className="pointer-events-none absolute inset-y-0 right-0 flex items-center pr-2">
-                            <ChevronUpDownIcon
-                              className="h-5 w-5 text-stone4"
-                              aria-hidden="true"
-                            />
-                          </span>
-                        </Listbox.Button>
-
-                        <Transition
-                          show={open}
-                          as={Fragment}
-                          leave="transition ease-in duration-100"
-                          leaveFrom="opacity-100"
-                          leaveTo="opacity-0"
-                        >
-                          <Listbox.Options className="absolute bg-stone5 z-10 mt-1 max-h-60 w-full overflow-auto rounded-md bg-white py-1 text-base shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none sm:text-sm">
-                            {setEventId(event.target.value).map((e) => (
-                              <Listbox.Option
-                                key={id}
-                                className={({ active }) =>
-                                  classNames(
-                                    active
-                                      ? "bg-orange text-white"
-                                      : "text-stone6",
-                                    "relative cursor-default select-none py-2 pl-3 pr-9"
-                                  )
-                                }
-                                value={event_id}
-                              >
-                                {({ selected, active }) => (
-                                  <>
-                                    <span
-                                      className={classNames(
-                                        selected
-                                          ? "font-semibold"
-                                          : "font-normal",
-                                        "block truncate"
-                                      )}
-                                    >
-                                      {setEventId(event.target.value)}
-                                    </span>
-
-                                    {selected ? (
-                                      <span
-                                        className={classNames(
-                                          active
-                                            ? "text-white"
-                                            : "text-orange",
-                                          "absolute inset-y-0 right-0 flex items-center pr-4"
-                                        )}
-                                      >
-                                        <CheckIcon
-                                          className="h-5 w-5"
-                                          aria-hidden="true"
-                                        />
-                                      </span>
-                                    ) : null}
-                                  </>
-                                )}
-                              </Listbox.Option>
-                            ))}
-                          </Listbox.Options>
-                        </Transition>
-                      </div>
-                    </>
-                  )}
-                </Listbox>
-              </div>
-            </div> */}
+          
             
             <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
               <label
                 htmlFor="event_id"
                 className="block text-sm font-medium leading-6 text-white sm:pt-1.5"
               >
-               Id del evento
+               Evento
               </label>
               <div className="mt-2 sm:col-span-2 sm:mt-0">
-                <input
+              <select
                   type="number"
                   name="event_id"
                   id="event_id"
@@ -230,7 +143,13 @@ const MolFormCodersEdit = ({ event }) => {
                   onChange={(event) => setEventId(event.target.value)}
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 py-1.5  text-stone6 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                />
+                >
+                  {event.map((e) => (
+                    <option key={e.id} value={e.id}>
+                      {e.name}
+                    </option>
+                  ))}
+                </select>
               </div>
             </div>
 
@@ -242,7 +161,7 @@ const MolFormCodersEdit = ({ event }) => {
                Id del promoción
               </label>
               <div className="mt-2 sm:col-span-2 sm:mt-0">
-                <input
+              <select
                   type="number"
                   name="promo_id"
                   id="promo_id"
@@ -250,27 +169,13 @@ const MolFormCodersEdit = ({ event }) => {
                   onChange={(event) => setPromoId(event.target.value)}
                   autoComplete="given-name"
                   className="block w-full rounded-md border-0 py-1.5  text-stone6 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                />
-              </div>
-            </div>
-
-            <div className="sm:grid sm:grid-cols-3 sm:items-start sm:gap-4 sm:py-6">
-              <label
-                htmlFor="province_id"
-                className="block text-sm font-medium leading-6 text-white sm:pt-1.5"
-              >
-               Id del provincia
-              </label>
-              <div className="mt-2 sm:col-span-2 sm:mt-0">
-                <input
-                  type="number"
-                  name="province_id"
-                  id="province_id"
-                  value={province_id ?? ""}
-                  onChange={(event) => setProvinceId(event.target.value)}
-                  autoComplete="given-name"
-                  className="block w-full rounded-md border-0 py-1.5  text-stone6 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:max-w-xs sm:text-sm sm:leading-6"
-                />
+                >
+                  {promotions.map((e) => (
+                    <option key={e.id} value={e.id}>
+                      {e.name}
+                    </option>
+                  ))}
+                </select>
               </div>
             </div>
 
