6893c7aaa501c76727475142bbd7a2a0fc3d2ae7
DianaB177 <anaidb77@gmail.com>
2023-04-18 12:10:05 +0200

    download successfully

---
+++ src/components/molecule/mol-excelUpDown/MolTableDoungLoad.jsx
@@ -0,0 +1,279 @@
+import {
+  getCompanies,
+  deleteCompanies,
+} from "../../../service/CompaniesService";
+import { useEffect, useLayoutEffect, useRef, useState } from "react";
+import { Link } from "react-router-dom";
+import { getProvinces } from "../../../service/ProvincesService";
+import MenuCompanies from "../mol-companies/MenuCompanies";
+import * as XLSX from "xlsx";
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+function exportToExcel(selectedCompanies) {
+  let selectedCompaniesForExport = [];
+  if (Array.isArray(selectedCompanies)) {
+    selectedCompaniesForExport = selectedCompanies.map(
+      ({ id, name, province, email, phone, priority }) => [id, name, province, email, phone, priority]
+    );
+  }
+
+  const sheet = XLSX.utils.aoa_to_sheet(selectedCompaniesForExport);
+
+  const book = {
+    Sheets: { data: sheet },
+    SheetNames: ["data"],
+  };
+  const bookType = "xlsx"; // fixed this line to specify the correct type
+  
+  const excelBuffer = XLSX.write(book, { // fixed this line to use XLSX.write instead of XLSX.writeFile
+    bookType: bookType,
+    type: "array",
+  });
+
+  const blob = new Blob([excelBuffer], { type: "application/octet-stream" });
+  const url = window.URL.createObjectURL(blob);
+  const a = document.createElement("a");
+  a.href = url;
+  a.download = "empresas.xlsx";
+  a.click();
+  window.URL.revokeObjectURL(url);
+}
+
+export default function MolTableDoungLoad() {
+  const checkbox = useRef();
+  const [checked, setChecked] = useState(false);
+  const [indeterminate, setIndeterminate] = useState(false);
+  const [selectedCompanies, setSelectedCompanies] = useState([]);
+  const [companies, setCompanies] = useState([]);
+  const [provinces, setProvinces] = useState([]);
+
+  useEffect(() => {
+    getCompanies()
+      .then((response) => {
+        setCompanies(response.data);
+        setSelectedCompanies(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useEffect(() => {
+    getProvinces()
+      .then((response) => {
+        setProvinces(response.data);
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  useLayoutEffect(() => {
+    const isIndeterminate =
+      selectedCompanies.length > 0 &&
+      selectedCompanies.length < companies.length;
+    setChecked(selectedCompanies.length === companies.length);
+    setIndeterminate(isIndeterminate);
+    checkbox.current.indeterminate = isIndeterminate;
+  }, [selectedCompanies, companies]);
+
+  function toggleAll() {
+    if (selectedCompanies.length === 0) {
+      return;
+    }
+    setSelectedCompanies(checked || indeterminate ? [] : selectedCompanies);
+    setChecked(!checked && !indeterminate);
+    setIndeterminate(false);
+  }
+
+  function handleDelete() {
+    if (selectedCompanies.length === 0) {
+      console.warn("No companies selected to delete");
+      return;
+    }
+    // Create an array of promises to delete each selected event
+    const deletePromises = selectedCompanies.map((company) =>
+      deleteCompanies(company.id)
+    );
+
+    // Delete all companies in parallel
+    Promise.all(deletePromises)
+      .then((responses) => {
+        console.log("Companies deleted successfully!");
+        // Remove all deleted companies from the companies state
+        const deletedIds = selectedCompanies.map((company) => company.id);
+        setCompanies(companies.filter((e) => !deletedIds.includes(e.id)));
+        // Clear the selectedCompanies state
+        setSelectedCompanies([]);
+        setChecked(false);
+        setIndeterminate(false);
+      })
+      .catch((error) => {
+        console.error(`Error deleting companies: ${error.message}`);
+      });
+  }
+  return (
+    <>
+      <MenuCompanies />
+      <div className="bg-stone6 w-screen max-w-screen-xl rounded-xl p-20 m-20 text-white">
+        <div className="sm:flex sm:items-center">
+          <div className="sm:flex-auto">
+            <h1 className="text-xl font-semibold leading-7">
+              Lista de empresas
+            </h1>
+          </div>
+          <div className="mt-4 sm:ml-16 sm:mt-0 sm:flex-none">
+            <button
+              className="text-sm text-stone2 my-10 mx-10 px-6 py-1.5 rounded-xl bg-gradient-to-r from-orangel to-orange hover:from-verde hover:to-verdel ..."
+              type="button"
+            >
+              <a href="/companiescreate">Crear empresa</a>
+            </button>
+          </div>
+        </div>
+        <div className="mt-8 flow-root">
+          <div className="-mx-4 -my-2 overflow-x-auto sm:-mx-6 lg:-mx-8">
+            <div className="inline-block min-w-full py-2 align-middle sm:px-6 lg:px-8">
+              <div className="relative">
+                {selectedCompanies.length > 0 && (
+                  <div className="block left-14 top-0 h-12 items-center space-x-3 sm:left-12">
+                    <button
+                      type="button"
+                      className="inline-flex items-center rounded px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
+                      onClick={() => handleDelete(selectedCompanies[0].id)}
+                    >
+                      Eliminar
+                    </button>
+                  </div>
+                )}
+                <table className="min-w-full table-fixed divide-y divide-gray-300">
+                  <thead>
+                    <tr>
+                      <th scope="col" className="relative px-7 sm:w-12 sm:px-6">
+                        <input
+                          type="checkbox"
+                          className="absolute left-4 top-1/2 -mt-2 h-4 w-4 rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                          ref={checkbox}
+                          checked={checked}
+                          onChange={toggleAll}
+                        />
+                      </th>
+                      <th
+                        scope="col"
+                        className="min-w-[12rem] py-3.5 pr-3 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Empresas
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Ubicación
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Provncia
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Email
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Télefono
+                      </th>
+                      <th
+                        scope="col"
+                        className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
+                      >
+                        Prioridad
+                      </th>
+                      <th
+                        scope="col"
+                        className="relative py-3.5 pl-3 pr-4 sm:pr-3"
+                      >
+                        <span className="sr-only">Editar</span>
+                      </th>
+                    </tr>
+                  </thead>
+
+                  <tbody className="divide-y divide-gray-200 ">
+                    {companies.map((e) => {
+                      // Buscar la provincia que corresponde a la empresa actual
+                      const province = provinces.find(
+                        (p) => p.id === e.province_id
+                      );
+                      return (
+                        <tr key={e.id}>
+                          <td className="px-7 py-4 whitespace-nowrap">
+                            <input
+                              type="checkbox"
+                              className="rounded border-gray-300 text-indigo-600 focus:ring-indigo-600"
+                              checked={selectedCompanies.includes(e)}
+                              onChange={(e) => {
+                                const isChecked = e.target.checked;
+                                setSelectedCompanies((prev) =>
+                                  isChecked
+                                    ? [...prev, e]
+                                    : prev.filter((c) => c !== e)
+                                );
+                              }}
+                            />
+                          </td>
+                          <td
+                            className={classNames(
+                              "whitespace-nowrap py-4 pr-3 text-sm font-medium",
+                              selectedCompanies.includes(e.id)
+                                ? "text-indigo-600"
+                                : "text-gray-900"
+                            )}
+                          >
+                            {e.name}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {e.ubication}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {province ? province.name : "-"}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {e.email}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {e.phone}
+                          </td>
+                          <td className="whitespace-nowrap px-3 py-4 text-sm text-gray-500">
+                            {e.priority}
+                          </td>
+                          <td className="px-6 py-4 whitespace-nowrap text-right text-sm font-medium">
+                            <Link
+                              to={`/companiesedit/${e.id}`}
+                              className="text-indigo-600 hover:text-indigo-900"
+                            >
+                              Editar
+                            </Link>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
+                <button
+                  className="inline-flex items-center rounded px-2 py-1 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50 disabled:cursor-not-allowed disabled:opacity-30 disabled:hover:bg-white"
+                  onClick={() => exportToExcel(selectedCompanies)}
+                >
+                  Descargar Excel
+                </button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
