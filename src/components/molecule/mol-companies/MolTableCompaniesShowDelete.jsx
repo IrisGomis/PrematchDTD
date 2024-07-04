a47bab9ecaec25fed2caa10ea4f6eedeaa4f4b77
DianaB177 <anaidb77@gmail.com>
2024-07-04 16:09:27 +0200

    update

--- src/components/molecule/mol-companies/MolTableCompaniesShowDelete.jsx
+++ src/components/molecule/mol-companies/MolTableCompaniesShowDelete.jsx
@@ -143,7 +143,7 @@ export default function MolTableCompaniesShowDelete() {
                         scope="col"
                         className="px-3 py-3.5 text-left text-sm font-semibold text-gray-900"
                       >
-                        Provncia
+                        Provincia
                       </th>
                       <th
                         scope="col"
